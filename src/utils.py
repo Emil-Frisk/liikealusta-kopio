3ef0c0826772ff3dfbfc96799b68fea137a427df
EmilFrisk1 <friskemil2@gmail.com>
2025-03-13 12:39:38 +0200

    lisäsin bitmap turvallisuus utility funktioita

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,18 @@
+FAULT_RESET_BIT = 15
+ENABLE_MAINTAINED_BIT = 1
+ALTERNATE_MODE_BIT = 7
+
 def is_nth_bit_on(n, number):
             mask = 1 << n
             return (number & mask) != 0
+
+# Only allows the needed bits
+def IEG_MODE_bitmask_default(number):
+        mask = (1 << FAULT_RESET_BIT) | (1 << ENABLE_MAINTAINED_BIT)
+        number = number & 0xFFFF
+        return number & mask
+
+def IEG_MODE_bitmask_alternative(number):
+        mask = (1 << FAULT_RESET_BIT) | (1 << ALTERNATE_MODE_BIT) |(1 << ENABLE_MAINTAINED_BIT) 
+        number = number & 0xFFFF
+        return number & mask
